b037338427b8856f07d86c80ec47f0885eca9968
duckhee <fain9301@yahoo.com>
2019-06-07 15:19:01 +0900

    set

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const IndexCtrl = require('../controller/index.controller.js');
+const getReq = require('request')
 
 
 /* GET home page. */
 router.get('/', IndexCtrl.Index);
+
+router.get('/device', (req, res, next) => {
+    var no = req.query.no || req.params.no || req.param.no || req.body.no;
+    getReq('http://www.iof.center/DataValue/getDeviceList10?no=' + no, (err, response, body) => {
+        console.log("response", body);
+        res.json(body);
+    });
+});
 
 module.exports = router;
