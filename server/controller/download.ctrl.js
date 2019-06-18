f86f49c90260407b7cee5877b7bb1d179bd34c9d
duckhee <fain9301@yahoo.com>
2019-06-18 19:09:06 +0900

    set download cal

---
+++ server/controller/download.ctrl.js
@@ -0,0 +1,8 @@
+const DownLoad = (req, res, next) => {
+    const id = req.body.bno || req.query.bno || req.params.bno || req.param.bno;
+
+};
+
+module.exports = {
+
+};
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const IndexCtrl = require('../controller/index.controller.js');
-
-
+const IndexCtrl = require("../controller/index.controller.js");
 
 /* GET home page. */
-router.get('/', IndexCtrl.Index);
+router.get("/", IndexCtrl.Index);
 
-router.get('/device', IndexCtrl.DeviceData);
+router.get("/device", IndexCtrl.DeviceData);
 
-router.get('/cellOne', IndexCtrl.CellOwn);
+router.get("/cellOne", IndexCtrl.CellOwn);
 
-router.get('/cellTwo', IndexCtrl.CellTwo);
+router.get("/cellTwo", IndexCtrl.CellTwo);
 
-router.get('/cellThree', IndexCtrl.CellThree);
+router.get("/cellThree", IndexCtrl.CellThree);
 
-router.get('/image', IndexCtrl.ImageData);
+router.get("/image", IndexCtrl.ImageData);
+
+router.get("/downloadData", (req, res, next) => {});
+
+router.post("/downloadData", (req, res, next) => {
+    const bno = req.query.no || req.body.no || req.param.no || req.params.no;
+    const startDate =
+        req.query.startCal || req.body.startCal || req.param.startCal || req.params.startCal;
+    const endDate =
+        req.query.endCal || req.body.endCal || req.param.endCal || req.params.endCal;
+    console.log("downloadData");
+    console.log("bno ::: "+bno+", start ::: " + startDate + ", end ::: " + endDate);
+    res.send("<script>history.back();</script>");
+});
 
 module.exports = router;
